5f0676fa718f355c53702b5964c6bc486660b9b8
Gabriel Tomio Nardes <gabrieltomio@protonmail.com>
2021-07-02 14:30:19 -0300

    feat: add usecases data

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,8 @@ export * from './domain/IdRequiredError'
 export * from './domain/UniqueEntityId'
 
 export * from './use-cases/Controller'
+export * from './use-cases/BodyError'
+export * from './use-cases/CreatedBody'
+export * from './use-cases/HttpResponse'
 
 export * from './helper/ArrayHelper'
