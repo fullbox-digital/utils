676b46f7adaa7cd544ffbd56d7c4912c71f97a62
FabricioSOliveira <fsoliveira.2908@gmail.com>
2021-07-28 16:24:35 -0300

    feat: add function split shift

--- src/core/DateTimeUtils.ts
+++ src/core/DateTimeUtils.ts
@@ -13,7 +13,35 @@ export abstract class DateTimeUtils {
     return new Date()
   }
 
-  static shift (date: Date, options: ShiftDateOptions): Date {
+  static shiftYears (date: Date, quantity: number): Date {
+    return this.shift(date, { year: quantity })
+  }
+
+  static shiftMonths (date: Date, quantity: number): Date {
+    return this.shift(date, { month: quantity })
+  }
+
+  static shiftDates (date: Date, quantity: number): Date {
+    return this.shift(date, { day: quantity })
+  }
+
+  static shiftHour (date: Date, quantity: number): Date {
+    return this.shift(date, { hour: quantity })
+  }
+
+  static shiftMinutes (date: Date, quantity: number): Date {
+    return this.shift(date, { minute: quantity })
+  }
+
+  static shiftSeconds (date: Date, quantity: number): Date {
+    return this.shift(date, { second: quantity })
+  }
+
+  static shiftMilliseconds (date: Date, quantity: number): Date {
+    return this.shift(date, { millisecond: quantity })
+  }
+
+  private static shift (date: Date, options: ShiftDateOptions): Date {
     return new Date(
       date.getFullYear() + (options.year ?? 0),
       date.getMonth() + (options.month ?? 0),
