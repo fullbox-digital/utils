6b19a60aa5962ddcdc884501ef2bbb065d2daf8c
Bruno Casas <bruno.casas2@gmail.com>
2021-08-20 17:13:18 -0300

    feat: add events to aggregate root

--- src/domain/AggregateRoot.ts
+++ src/domain/AggregateRoot.ts
@@ -2,19 +2,30 @@ import { DomainEvent } from './DomainEvent'
 import { EventStore } from './EventStore'
 import { Entity } from './Entity'
 
-export abstract class AggregateRoot<T> extends Entity<T> {
+type Events<T> = Array<{ date: number } & T>
+
+export abstract class AggregateRoot<T, E = unknown> extends Entity<T> {
   private readonly domainEvents: DomainEvent[] = []
+  private readonly events: Events<E> = []
 
   getDomainEvents (): DomainEvent[] {
     return this.domainEvents
   }
 
-  protected addDomainEvent (domainEvent: DomainEvent): void {
+  protected addDomainEvent (domainEvent: DomainEvent, event?: E): void {
     this.domainEvents.push(domainEvent)
+    if (event) {
+      this.events.push({
+        date: domainEvent.dateTimeOccurred.getTime(),
+        ...event
+      })
+    }
     EventStore.markAggregateForDispatch(this)
   }
 
   clearEvents (): void {
     this.domainEvents.splice(0, this.domainEvents.length)
   }
+
+  getEvents (): E[] { return this.events }
 }
