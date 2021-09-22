b8e2d5a3544a8a85acce37fee1c6409f54a502c2
Gabriel Tomio <gabrieltomio@protonmail.com>
2021-09-22 11:37:00 -0300

    feat: validation error receive a list of validation error

--- src/validation/ValidationError.ts
+++ src/validation/ValidationError.ts
@@ -1,8 +1,8 @@
 import { BusinessError } from '..'
 
 export class ValidationError extends BusinessError {
-  constructor (message: string) {
-    super(message)
+  constructor (message: string | ValidationError[]) {
+    super(typeof message === 'string' ? message : message.map(e => e.message).join(', '))
     this.name = 'ValidationError'
   }
 }
