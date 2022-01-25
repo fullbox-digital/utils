4192d3ce70d42f88c0b7f467ff1e55262dc491c0
FabricioSOliveira <fsoliveira.2908@gmail.com>
2022-01-25 09:15:06 -0300

    feat: add download-by-url

--- src/domain/Action.ts
+++ src/domain/Action.ts
@@ -1,7 +1,7 @@
 export interface Action {
   name: string
   path: string
-  method: 'post' | 'put' | 'delete' | 'get' | 'navigation' | 'download'
+  method: 'post' | 'put' | 'delete' | 'get' | 'navigation' | 'download' | 'download-by-url'
 }
 
 export type ActionList<T> = T & { actions: Action[] }
