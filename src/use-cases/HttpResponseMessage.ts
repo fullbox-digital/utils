0fbfc696ab90b1398b277b482c386697434ff1ca
Matheus Jones Seberino da Silva <matheusjones295@gmail.com>
2022-02-07 10:35:33 -0300

    fix: fix type of HttpResponseMessage

--- src/use-cases/HttpResponseMessage.ts
+++ src/use-cases/HttpResponseMessage.ts
@@ -1,28 +1,28 @@
-import { AlertBody, HttpStatus, InfoBody, ResponseType, SuccessBody, WarningBody } from '..'
+import { AlertBody, HttpResponse, HttpStatus, InfoBody, ResponseType, SuccessBody, WarningBody } from '..'
 
 export abstract class HttpResponseMessage {
-  static success = (message: string): HttpStatus => {
+  static success = (message: string): HttpResponse<SuccessBody> => {
     return HttpStatus.ok<SuccessBody>({
       type: ResponseType.SUCCESS,
       message
     })
   }
 
-  static warning = (message: string): HttpStatus => {
+  static warning = (message: string): HttpResponse<WarningBody> => {
     return HttpStatus.ok<WarningBody>({
       type: ResponseType.WARNING,
       message
     })
   }
 
-  static alert = (message: string): HttpStatus => {
+  static alert = (message: string): HttpResponse<AlertBody> => {
     return HttpStatus.ok<AlertBody>({
       type: ResponseType.ALERT,
       message
     })
   }
 
-  static info = (message: string): HttpStatus => {
+  static info = (message: string): HttpResponse<InfoBody> => {
     return HttpStatus.ok<InfoBody>({
       type: ResponseType.INFO,
       message
