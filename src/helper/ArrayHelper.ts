b1550c5003900f77499c37d5e81bb1cf9d0ac939
Felipe Borba <felipe.borba0109@gmail.com>
2022-04-27 13:48:05 -0300

    feat: add function to find repeated items in array

--- src/helper/ArrayHelper.ts
+++ src/helper/ArrayHelper.ts
@@ -1,6 +1,14 @@
 export abstract class ArrayHelper {
-  static randomSelect<T> (...array: T[]): T {
+  static randomSelect<T>(...array: T[]): T {
     const index = Math.floor(Math.random() * array.length)
     return array[index]
+  }
+
+  static findDuplicate (arr: string[]): string[] {
+    const found: { [key: string]: boolean } = {}
+    arr.forEach((item) => {
+      found[item] = typeof found[item] === 'boolean'
+    })
+    return Object.keys(found).filter((key) => found[key])
   }
 }
